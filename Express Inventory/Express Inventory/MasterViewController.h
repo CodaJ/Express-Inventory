649e729484cddaa102d976a9e2cf22f1e4a311ac
Coda Jones <nekotia@gmail.com>
2012-03-14 18:14:51 -0500

    first commit of project files

---
+++ Express Inventory/Express Inventory/MasterViewController.h
@@ -0,0 +1,22 @@
+//
+//  MasterViewController.h
+//  Express Inventory
+//
+//  Created by Nekotia Jones on 3/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DetailViewController;
+
+#import <CoreData/CoreData.h>
+
+@interface MasterViewController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property (strong, nonatomic) DetailViewController *detailViewController;
+
+@property (strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+@property (strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+
+@end
